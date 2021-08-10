6123f40ff79d31cbd670090d93ada72978931ca1
Diego Orellana <d7shaal@gmail.com>
2021-08-10 12:51:44 -0600

    Every new color now is saved in a variable

--- main.js
+++ main.js
@@ -6,12 +6,31 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
   let hexColor = "#";
   for (let i = 0; i < 6; i++) {
-    hexColor += hexaCodes[getRandomNumber()];
+    hexColor += hexaCodes[random()];
   }
+  let hexColors = [];
+  let codes = hexColors.push(hexColor);
+
+  console.log("New color: ", hexColor);
+  console.log(hexColors);
+  console.log("Value of color in array: ", hexColors.indexOf(hexColor));
+  console.log("Quantity of codes in array: ", codes);
+  console.log("Second time: ", hexColor);
+
   color.textContent = hexColor;
   document.querySelector(".colorWrapper").style.backgroundColor = hexColor;
 });
 
-function getRandomNumber() {
+let random = function getRandomNumber() {
   return Math.floor(Math.random() * hexaCodes.length);
+};
+
+// console.log(hexColors[0]);
+
+function colorBackward() {
+  // hexColors.push[];
+}
+
+function colorForward() {
+  console.log("test");
 }
