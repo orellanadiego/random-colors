9e6cefe18e2bc2f6a161e0de94fa0cbd946f2216
Diego Orellana <d7shaal@gmail.com>
2021-08-26 15:51:07 -0600

    all code is sorted, main function was modified and simplified

--- main.js
+++ main.js
@@ -1,36 +1,56 @@
-let hexaCodes = ["A", "B", "C", "D", "E", "F", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+/* =======
+ALL VARIABLES
+======= */
+const hexaCodes = ["A", "B", "C", "D", "E", "F", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const btn = document.querySelector(".btn");
 const color = document.querySelector(".color");
+const backward = document.querySelector(".btn-backward");
+const forward = document.querySelector(".btn-forward");
+const hexColors = [];
 
 btn.addEventListener("click", function () {
-  let hexColor = "#";
-  for (let i = 0; i < 6; i++) {
-    hexColor += hexaCodes[random()];
-  }
-  let hexColors = [];
-  let codes = hexColors.push(hexColor);
-
-  console.log("New color: ", hexColor);
-  console.log(hexColors);
-  console.log("Value of color in array: ", hexColors.indexOf(hexColor));
-  console.log("Quantity of codes in array: ", codes);
-  console.log("Second time: ", hexColor);
-
-  color.textContent = hexColor;
-  document.querySelector(".colorWrapper").style.backgroundColor = hexColor;
+  generateColor();
 });
 
-let random = function getRandomNumber() {
-  return Math.floor(Math.random() * hexaCodes.length);
-};
+/* =======
+BUTTON LISTENERS
+======= */
+backward.addEventListener("click", function () {
+  colorBackward();
+});
 
-// console.log(hexColors[0]);
+forward.addEventListener("click", function () {
+  colorForward();
+});
+
+/* =======
+ALL FUNCTIONS
+======= */
+function generateColor() {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hexaCodes[getRandomNumber()];
+  }
+  hexColors.push(hexColor);
+
+  console.log("====");
+  console.log("New color: ", hexColor);
+  console.log(hexColors);
+  console.log("Value of color in array: ", Object.keys(hexColors));
+  console.log("Quantity of codes in array: ", hexColors.length);
+  console.log("Second time: ", hexColor);
+  color.textContent = hexColor;
+  document.querySelector(".colorWrapper").style.backgroundColor = hexColor;
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hexaCodes.length);
+}
 
 function colorBackward() {
-  // hexColors.push[];
+  console.log("This is the backward button!");
 }
 
 function colorForward() {
-  console.log("test");
+  console.log("This is the forward button!");
 }
